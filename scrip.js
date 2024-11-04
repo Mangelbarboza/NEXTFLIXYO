8ee7c7c39e5cd58dd94f11c928f1c51161788105
Angel Barboza <144569016+Mangelbarboza@users.noreply.github.com>
2024-11-03 22:34:16 -0600

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginButton = document.querySelector(".btn-login");
+
+    loginButton.addEventListener("click", () => {
+        alert("Función de inicio de sesión en construcción.");
+    });
+
+    const watchNowButton = document.querySelector(".btn-primary");
+
+    watchNowButton.addEventListener("click", () => {
+        alert("Función de ver ahora");
+    });
+});
